0001acf7bb5ab8ee5955ab7f3548c5c6d350bc8a
Hashem Afkhami <hashem_afkhami@yahoo.com>
2025-10-08 22:03:09 +0330

    set version PrestaSDK

--- src/LocalizerModule.php
+++ src/LocalizerModule.php
@@ -13,7 +13,7 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-use PrestaSDK\V040\PrestaSDKModule;
+use PrestaSDK\V071\PrestaSDKModule;
 use PrestaYar\Localizer\Native\NativeCorePrestashop;
 use PrestaYar\Localizer\Traits\UseDate;
 
